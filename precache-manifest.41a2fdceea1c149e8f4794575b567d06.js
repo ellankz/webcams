054f1587f25e56638677420e86a89730394d7ead
ellankz <enelipa@gmail.com>
2020-03-16 16:51:29 +0200

    Updates

---
+++ precache-manifest.41a2fdceea1c149e8f4794575b567d06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b16970893c6b8d7df8dd07cfe50f71",
+    "url": "/webcams/index.html"
+  },
+  {
+    "revision": "309480f1ebb1775b6a6b",
+    "url": "/webcams/static/css/main.bbb92989.chunk.css"
+  },
+  {
+    "revision": "70f3449821869f4ef63a",
+    "url": "/webcams/static/js/2.9f4f5714.chunk.js"
+  },
+  {
+    "revision": "309480f1ebb1775b6a6b",
+    "url": "/webcams/static/js/main.a632bd94.chunk.js"
+  },
+  {
+    "revision": "fdd99d6e0c91c708c8e0",
+    "url": "/webcams/static/js/runtime-main.041146df.js"
+  }
+]);
